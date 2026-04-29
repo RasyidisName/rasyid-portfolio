377117d04cf59ba4e1ee47606a3965f036d205c0
RasyidisName <contact.rassyid@gmail.com>
2026-04-29 14:51:27 +0700

    feat: initial hero section with tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tailwind v4 is Active!
+      </h1>
+    </div>
+  )
+}
