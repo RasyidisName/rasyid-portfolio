8bf7ca27095ae5bbf83ebd28b9553a06ac5196b6
RasyidisName <contact.rassyid@gmail.com>
2026-05-14 17:12:39 +0700

    feat: integrate non-blocking google tag manager container

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module'
+
+// Konfigurasi GTM Non-blocking
+const tagManagerArgs = {
+    gtmId: 'GTM-XXXXXXX' // 
+}
+
+TagManager.initialize(tagManagerArgs)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
